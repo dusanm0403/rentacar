2f91fae576c1116eb054512d3b4d113777998baa
dusanm0403 <dusanm0403@gmail.com>
2021-02-05 15:45:59 +0100

    navigation wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Navigation from "./components/navigation";
 import "./styles/base/app.scss";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
 };
 
 export default App;
--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./Navigation.scss";
 
 const Navigation = () => {
-  return <div className="Navigation" />;
+  return (
+    <div className="Navigation">
+      <div className="Navigation-left">
+        <h2>vozni park</h2>
+        <h2>o nama</h2>
+      </div>
+      <div className="Navigation-center">
+        <h3>Logo</h3>
+      </div>
+      <div className="Navigation-right">
+        <h2>cenovnik</h2>
+        <h2>kontakt</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Navigation;
